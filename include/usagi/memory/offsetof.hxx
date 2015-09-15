a3b2acc1a28546e866dd99c13bd7ce183eb145e7
Usagi Ito <usagi@WonderRabbitProject.net>
2015-09-15 10:53:46 +0900

    add memory/offsetof.hxx

---
+++ include/usagi/memory/offsetof.hxx
@@ -0,0 +1,15 @@
+/// @file
+/// @brief POD型のエレメントのアドレスオフセットを得る OFFSETOF マクロを定義する
+
+#pragma once
+
+#include <cstdint>
+
+#ifndef OFFSETOF
+  /// @def OFFSETOF( TYPE, ELEMENT )
+  /// @brief POD型のELEMENT要素のアドレスオフセットを std::size_t 型で取得
+  /// @param TYPE 要素のアドレスオフセットを得るPOD型
+  /// @param ELEMENT アドレスオフセットを得るTYPE型の要素
+  /// @return アドレスオフセット
+  #define OFFSETOF( TYPE, ELEMENT ) ( reinterpret_cast< std::size_t >( &reinterpret_cast< TYPE * >( 0 ) -> ELEMENT ) )
+#endif
