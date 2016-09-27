fc2b33100d12d19b16339c0bfcc3a67612f56cb6
Usagi Ito <usagi@WonderRabbitProject.net>
2016-09-27 16:35:15 +0900

    add support tooltip

--- include/usagi/imgui/small_button_cloud.hxx
+++ include/usagi/imgui/small_button_cloud.hxx
@@ -8,14 +8,44 @@ namespace usagi
 {
   namespace imgui
   {
-    class small_button_cloud
-      : public std::multimap
+    using small_button_cloud_functor_type
+      = std::function< auto () -> void >;
+    
+    using small_button_cloud_base_type
+      = std::multimap
         < std::string
-        , std::function< auto () -> void >
+        , std::tuple
+          < small_button_cloud_functor_type
+          , std::string
+          >
         >
+      ;
+    
+    class small_button_cloud
+      : public small_button_cloud_base_type
     {
       float _width = 0.0f;
     public:
+      using functor_type = std::function< auto () -> void >;
+      
+      auto emplace( std::string&& key, small_button_cloud_functor_type&& f )
+      { return small_button_cloud_base_type::emplace( key, std::make_tuple( f, std::string() ) ); }
+      
+      auto emplace( std::string&& key, std::string&& description )
+      { return small_button_cloud_base_type::emplace( key, std::make_tuple( []{}, description ) ); }
+      
+      auto emplace( std::string&& key, small_button_cloud_functor_type&& f, std::string&& description )
+      { return small_button_cloud_base_type::emplace( key, std::make_tuple( f, description ) ); }
+      
+      auto insert( const std::string& key, const small_button_cloud_functor_type& f )
+      { return small_button_cloud_base_type::emplace( key, std::make_tuple( f, std::string() ) ); }
+      
+      auto insert( const std::string& key, const std::string& description )
+      { return small_button_cloud_base_type::emplace( key, std::make_tuple( []{}, description ) ); }
+      
+      auto insert( const std::string& key, const small_button_cloud_functor_type& f, const std::string& description )
+      { return small_button_cloud_base_type::emplace( key, std::make_tuple( f, description ) ); }
+      
       auto width() { return _width; }
       auto width( const decltype( _width ) in ) { return _width = in; }
       auto get_width_pointer() { return &_width; }
@@ -43,7 +73,12 @@ namespace usagi
             current_line_width = iis2 + entity_width;
           
           if ( ImGui::SmallButton( p.first.c_str() ) )
-            p.second();
+            std::get< small_button_cloud_functor_type >( p.second )();
+          
+          const auto& description = std::get< std::string >( p.second );
+          
+          if ( not description.empty() and ImGui::IsItemHovered() )
+            ImGui::SetTooltip( description.c_str() );
         }
         
         ImGui::EndGroup();
