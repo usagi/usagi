2bfeb734b7cf7a39a0fbf9c16f1d91cf13792ef8
Usagi Ito <usagi@WonderRabbitProject.net>
2017-02-03 20:01:33 +0900

    update

--- include/usagi/json/picojson/to_bool.hxx
+++ include/usagi/json/picojson/to_bool.hxx
@@ -7,32 +7,56 @@
 
 namespace usagi::json::picojson
 {
+  static inline auto to_bool( const boolean_type in ) { return in; }
+  
+  /// @note ECMA-262 NaN: Boolean( 0/0 ) -> false
+  /// @note ECMA-262 +Inf: Boolean( +1/0 ) -> true
+  /// @note ECMA-262 -Inf: Boolean( -1/0 ) -> true
+  static inline auto to_bool( const number_type in )
+  {
+    return not std::isnan( in ) and in != 0;
+  }
+  
+  /// @note ECMA-262 empty-string: Boolean( "" ) -> false
+  static inline auto to_bool( const string_type& in )
+  {
+    return not in.empty();
+  }
+  
+  /// @note: ECMA-262 array: Boolean( [] ) -> true
+  static inline auto to_bool( const array_type& )
+  { return true; }
+  
+  /// @note: ECMA-262 object: Boolean( {} ) -> true
+  static inline auto to_bool( const object_type& )
+  { return true; }
+  
+  /// @note: ECMA-262 null: Boolean( null ) -> false
+  static inline auto to_bool( const null_type& = null_type() )
+  { return false; }
+  
   /// @brief ECMA-262 Boolean( in ) 互換変換
   static inline auto to_bool( const value_type& in )
   {
     if ( in.is< boolean_type >() )
-      return in.get< boolean_type >();
+      return to_bool( in.get< boolean_type >() );
     
     if ( in.is< number_type >() )
-    {
-      const auto v = in.get< number_type >();
-      // ECMA-262 NaN: Boolean( 0/0 ) -> false
-      // ECMA-262 +Inf: Boolean( +1/0 ) -> true
-      // ECMA-262 -Inf: Boolean( -1/0 ) -> true
-      return not std::isnan( v ) and v != 0;
-    }
+      return to_bool( in.get< number_type >() );
     
     if ( in.is< string_type >() )
-    {
-      const auto& v = in.get< string_type >();
-      // ECMA-262 empty-string: Boolean( "" ) -> false
-      return not v.empty();
-    }
+      return to_bool( in.get< string_type >() );
     
-    // ECMA-262 object: Boolean( {} ) -> true
-    // ECMA-262 array: Boolean( [] ) -> true
-    // ECMA-262 null: Boolean( null ) -> false
-    return in.is< array_type >() or in.is< object_type >();
+    if ( in.is< array_type >() )
+      return to_bool( in.get< array_type >() );
+    
+    if ( in.is< object_type >() )
+      return to_bool( in.get< object_type >() );
+    
+    if ( in.is< null_type >() )
+      return to_bool();
+    
+    return false;
   }
   
   /// @brief to_bool した結果を value_type で得る syntax sugar
