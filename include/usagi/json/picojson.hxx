ed52aa42a6d8394a6273c36c91dbc9528add56fd
Usagi Ito <usagi@WonderRabbitProject.net>
2017-02-07 19:17:37 +0900

    抜け修正

--- include/usagi/json/picojson.hxx
+++ include/usagi/json/picojson.hxx
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "picojson/type.hxx"
+#include "picojson/make_array.hxx"
 #include "picojson/make_object.hxx"
 #include "picojson/get_value.hxx"
 #include "picojson/set_value.hxx"
